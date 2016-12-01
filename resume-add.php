a0ce7424dbad5b4335668989e32ea4094cdb1ef7
Belmin 2 <belminkrak479@gmail.com>
2016-12-01 22:53:44 +0100

    add changes

--- resume-add.php
+++ resume-add.php
@@ -12,88 +12,6 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
-
-
-<?php
-   if(isset($_FILES['image'])){
-      $errors= array();
-      $file_name = $_FILES['image']['name'];
-      $file_size =$_FILES['image']['size'];
-      $file_tmp =$_FILES['image']['tmp_name'];
-      $file_type=$_FILES['image']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
-      
-      $expensions= array("jpeg","jpg","png");
-      
-      if(in_array($file_ext,$expensions)=== false){
-         $errors[]="";
-      }
-      
-      if($file_size > 2097152){
-         $errors[]='File size must be excately 2 MB';
-      }
-      
-      if(empty($errors)==true){
-         move_uploaded_file($file_tmp,"resume_img/".$file_name);
-         echo "<h3 style='text-align:center;color:#0000;'>Your picture has been successfully saved</h3>";
-      }else{
-         print_r($errors);
-      }
-if (empty($file_name)) {
-  echo "<h2 style='text-align:center;'>Please add your image</h2>";
-} else {
- 
-
-
-
-  $sql = "INSERT INTO images (img) VALUES('$file_name')";
-$result = mysqli_query($conn,$sql);
-  
-}
-   }
-?>
-
-
-<?php
-if (isset($_POST['submit'])) {
-$name = $_POST["name"];
-$headline = $_POST["headline"];
-$short_description = $_POST["short_description"];
-$location = $_POST["location"];
-$page_url = $_POST["page_url"];
-$salary = $_POST["salary"];
-$age = $_POST["age"];
-$contact =$_POST["contact"];
-$email = $_POST["email"];
-$tag_name = $_POST["tag_name"];
-if (empty($name) || empty($headline) || empty($short_description) || empty($location) || empty($page_url)|| empty($salary)|| empty($age) || empty($contact)|| empty($email)||empty($tag_name)) {
-
-echo "<h3 style='text-align:center;color:#0000;font-family:Tahoma;'>You did not fill out the requierd fields<h3>";
-
-}
-
-else {
-
-$sql = "INSERT INTO resume (name,headline,short_description,location,page_url,salary,age,contact,email,tag_name) VALUES ('$_POST[name]','$_POST[headline]','$_POST[short_description]','$_POST[location]','$_POST[page_url]','$_POST[salary]','$_POST[age]','$_POST[contact]','$_POST[email]','$_POST[tag_name]')";
-$result = mysqli_query($conn,$sql);
-if ($result) {
-  echo "Succes entered ";
-} else {
-  echo "Error";
-}
-}
-
-}
-
-
-
-
-
-
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -147,7 +65,7 @@ if ($result) {
             </a>
 
             <ul class="dropdown-menu dropdown-menu-right">
-              <li><a href="user-login.html">Login</a></li>
+              <li><a href="user-login.php">Login</a></li>
               <li><a href="user-register.php">Register</a></li>
               <li><a href="user-forget-pass.php">Forget pass</a></li>
               <li><a href="#">Logout</a></li>
@@ -231,6 +149,102 @@ if ($result) {
 
         <div class="container">
 
+
+<?php
+   if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size =$_FILES['image']['size'];
+      $file_tmp =$_FILES['image']['tmp_name'];
+      $file_type=$_FILES['image']['type'];
+      $img_path= "resume_img/".$file_name;
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+      $expensions= array("jpeg","jpg","png");
+      
+      if(in_array($file_ext,$expensions)=== false){
+         $errors[]="";
+      }
+      
+      if($file_size > 2097152){
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"resume_img/".$file_name);
+         echo "<h3 style='text-align:center;color:#0000;'>Your picture has been successfully saved</h3>";
+      }else{
+         print_r($errors);
+      }
+if (empty($file_name)) {
+  echo "<h2 style='text-align:center;'>Please add your image</h2>";
+} else {
+ 
+
+
+
+  $sql = "INSERT INTO resume(profile_img) VALUES('$img_path')";
+$result = mysqli_query($conn,$sql);
+  
+}
+   }
+?>
+
+
+<?php
+if (isset($_POST['submit'])) {
+$name = $_POST["name"];
+$headline = $_POST["headline"];
+$short_description = $_POST["short_description"];
+$location = $_POST["location"];
+$page_url = $_POST["page_url"];
+$salary = $_POST["salary"];
+$age = $_POST["age"];
+$contact =$_POST["contact"];
+$email = $_POST["email"];
+$tag_name = $_POST["tag_name"];
+$degree_bachelor = $_POST["degree_bachelor"];
+$major_computer_sience = $_POST["major_computer_sience"];
+$school_name = $_POST["school_name"];
+$date_from = $_POST["date_from"];
+$date_to = $_POST["date_to"];
+$education_discription = $_POST["education_discription"];
+$company_name = $_POST["company_name"];
+$position = $_POST["position"];
+$work_date_from = $_POST["work_date_from"];
+$work_date_to = $_POST["work_date_to"];
+$work_description = $_POST["work_description"];
+$skill_name = $_POST["skill_name"];
+$skill_proficiency = $_POST["skill_proficiency"];
+
+
+
+if (empty($name) || empty($headline) || empty($short_description) || empty($location) || empty($page_url)|| empty($salary)|| empty($age) || empty($contact)|| empty($email)||empty($tag_name) || empty($degree_bachelor) || empty($major_computer_sience) || empty($school_name) || empty($date_from)||empty($date_to) || empty($education_discription) ||empty($company_name)|| empty($position) || empty($work_date_from)||empty($work_date_to) || empty($work_description) || empty($skill_name) || empty($skill_proficiency)) {
+
+echo "<h3 style='text-align:center;color:#0000;'>You did not fill out the requierd fields<h3>";
+
+}
+
+else {
+
+$sql = "INSERT INTO resume (name,headline,short_description,location,page_url,salary,age,contact,email,tag_name,degree_bachelor,major_computer_sience,school_name,date_from,date_to,education_discription,company_name,position,work_date_from,work_date_to,work_description,skill_name,skill_proficiency) VALUES ('$_POST[name]','$_POST[headline]','$_POST[short_description]','$_POST[location]','$_POST[page_url]','$_POST[salary]','$_POST[age]','$_POST[contact]','$_POST[email]','$_POST[tag_name]','$_POST[degree_bachelor]','$_POST[major_computer_sience]','$_POST[school_name]','$_POST[date_from]','$_POST[date_to]','$_POST[education_discription]','$_POST[company_name]','$_POST[position]','$_POST[work_date_from]','$_POST[work_date_to]','$_POST[work_description]','$_POST[skill_name]','$_POST[skill_proficiency]')";
+$result = mysqli_query($conn,$sql);
+if ($result) {
+  echo "<h3 style='text-align:center;color:#0000;'>Succes entered<h3>";
+
+} else {
+  echo "Error";
+}
+}
+
+}
+
+
+
+
+
+
+?>
           <div class="row">
             <div class="col-xs-12 col-sm-4">
               <div class="form-group">
@@ -346,7 +360,45 @@ if ($result) {
             
             <div class="row">
 
+<?php
+   if(isset($_FILES['logo1'])){
+      $errors= array();
+      $file_name = $_FILES['logo1']['name'];
+      $file_size =$_FILES['logo1']['size'];
+      $file_tmp =$_FILES['logo1']['tmp_name'];
+      $file_type=$_FILES['logo1']['type'];
+      $img_path= "resume_img/".$file_name;
+      $file_ext=strtolower(end(explode('.',$_FILES['logo1']['name'])));
+      
+      $expensions= array("jpeg","jpg","png");
+      
+      if(in_array($file_ext,$expensions)=== false){
+         $errors[]="";
+      }
+      
+      if($file_size > 2097152){
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"resume_img/".$file_name);
+         echo "<h3 style='text-align:center;color:#0000;'>Your logo has been successfully saved</h3>";
+      }else{
+         print_r($errors);
+      }
+if (empty($file_name)) {
+  echo "<h2 style='text-align:center;'>Please add your logo</h2>";
+} else {
+ 
 
+
+
+  $sql = "INSERT INTO resume(education_img) VALUES('$img_path')";
+$result = mysqli_query($conn,$sql);
+  
+}
+   }
+?>
 
               <div class="col-xs-12">
                 <div class="item-block">
@@ -355,36 +407,37 @@ if ($result) {
                     <button class="btn btn-danger btn-float btn-remove"><i class="ti-close"></i></button>
 
                     <div class="row">
+  
                       <div class="col-xs-12 col-sm-4">
                         <div class="form-group">
-                          <input type="file" name="logo" class="dropify" data-default-file="./pictures/logo-default.png">
+                          <input type="file" name="logo1" class="dropify" data-default-file="./pictures/logo-default.png">
                           <span class="help-block">Please choose a square logo</span>
                         </div>
                       </div>
 
                       <div class="col-xs-12 col-sm-8">
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Degree, e.g. Bachelor">
+                          <input type="text" name="degree_bachelor" class="form-control" placeholder="Degree, e.g. Bachelor">
                         </div>
 
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Major, e.g. Computer Science">
+                          <input type="text" name="major_computer_sience" class="form-control" placeholder="Major, e.g. Computer Science">
                         </div>
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="School name, e.g. Massachusetts Institute of Technology">
+                          <input type="text"  name="school_name" class="form-control" placeholder="School name, e.g. Massachusetts Institute of Technology">
                         </div>
 
                         <div class="form-group">
                           <div class="input-group">
                             <span class="input-group-addon">Date from</span>
-                            <input type="text" class="form-control" placeholder="e.g. 2012">
+                            <input type="text" name="date_from" class="form-control" placeholder="e.g. 2012">
                             <span class="input-group-addon">Date to</span>
-                            <input type="text" class="form-control" placeholder="e.g. 2016">
+                            <input type="text" name="date_to" class="form-control" placeholder="e.g. 2016">
                           </div>
                         </div>
 
                         <div class="form-group">
-                          <textarea class="form-control" rows="3" placeholder="Short description"></textarea>
+                          <textarea class="form-control" rows="3" name="education_discription" placeholder="Short description"></textarea>
                         </div>
                       </div>
                     </div>
@@ -402,34 +455,35 @@ if ($result) {
                     <div class="row">
                       <div class="col-xs-12 col-sm-4">
                         <div class="form-group">
-                          <input type="file" class="dropify" data-default-file="assets/img/logo-default.png">
+                           <input type="file" name="logo1c" class="dropify" data-default-file="./pictures/logo-default.png">
                           <span class="help-block">Please choose a square logo</span>
                         </div>
                       </div>
 
                       <div class="col-xs-12 col-sm-8">
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Degree, e.g. Bachelor">
+                          <input type="text" name="degree_bachelor1" class="form-control" placeholder="Degree, e.g. Bachelor">
                         </div>
 
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Major, e.g. Computer Science">
+                          <input type="text" name="major_computer_sience1" class="form-control" placeholder="Major, e.g. Computer Science">
                         </div>
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="School name, e.g. Massachusetts Institute of Technology">
+                          <input type="text"  name="school_name1" class="form-control" placeholder="School name, e.g. Massachusetts Institute of Technology">
                         </div>
 
                         <div class="form-group">
                           <div class="input-group">
                             <span class="input-group-addon">Date from</span>
-                            <input type="text" class="form-control" placeholder="e.g. 2012">
+                            <input type="text" name="date_from1" class="form-control" placeholder="e.g. 2012">
                             <span class="input-group-addon">Date to</span>
-                            <input type="text" class="form-control" placeholder="e.g. 2016">
+                            <input type="text" name="date_to1" class="form-control" placeholder="e.g. 2016">
                           </div>
                         </div>
 
                         <div class="form-group">
-                          <textarea class="form-control" rows="3" placeholder="Short description"></textarea>
+                       <textarea class="form-control" rows="3" name="education_discription1" placeholder="Short description"></textarea>
+
                         </div>
                       </div>
                     </div>
@@ -459,7 +513,45 @@ if ($result) {
             </header>
             
             <div class="row">
+                   <?php
+   if(isset($_FILES['logo2'])){
+      $errors= array();
+      $file_name = $_FILES['logo2']['name'];
+      $file_size =$_FILES['logo2']['size'];
+      $file_tmp =$_FILES['logo2']['tmp_name'];
+      $file_type=$_FILES['logo2']['type'];
+      $img_path= "resume_img/".$file_name;
+      $file_ext=strtolower(end(explode('.',$_FILES['logo2']['name'])));
+      
+      $expensions= array("jpeg","jpg","png");
+      
+      if(in_array($file_ext,$expensions)=== false){
+         $errors[]="";
+      }
+      
+      if($file_size > 2097152){
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"resume_img/".$file_name);
+         echo "<h3 style='text-align:center;color:#0000;'>Your logo has been successfully saved</h3>";
+      }else{
+         print_r($errors);
+      }
+if (empty($file_name)) {
+  echo "<h2 style='text-align:center;'>Please add your logo</h2>";
+} else {
+ 
 
+
+
+  $sql = "INSERT INTO resume (work_img) VALUES('$img_path')";
+$result = mysqli_query($conn,$sql);
+  
+}
+   }
+?>
               <div class="col-xs-12">
                 <div class="item-block">
                   <div class="item-form">
@@ -476,19 +568,19 @@ if ($result) {
 
                       <div class="col-xs-12 col-sm-8">
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Company name">
+                          <input type="text" name="company_name" class="form-control" placeholder="Company name">
                         </div>
 
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Position, e.g. UI/UX Researcher">
+                          <input name="position" type="text" class="form-control" placeholder="Position, e.g. UI/UX Researcher">
                         </div>
 
                         <div class="form-group">
                           <div class="input-group">
                             <span class="input-group-addon">Date from</span>
-                            <input type="text" class="form-control" placeholder="e.g. 2012">
+                            <input name="work_date_from" type="text" class="form-control" placeholder="e.g. 2012">
                             <span class="input-group-addon">Date to</span>
-                            <input type="text" class="form-control" placeholder="e.g. 2016">
+                            <input name="work_date_to" type="text" class="form-control" placeholder="e.g. 2016">
                           </div>
                         </div>
 
@@ -496,7 +588,7 @@ if ($result) {
 
                       <div class="col-xs-12">
                         <div class="form-group">
-                          <textarea class="summernote-editor"></textarea>
+                          <textarea name="work_description" class="summernote-editor"></textarea>
                         </div>
                       </div>
                     </div>
@@ -582,7 +674,7 @@ if ($result) {
                     <div class="row">
                       <div class="col-xs-12 col-sm-6">
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Skill name, e.g. HTML">
+                          <input name="skill_name" type="text" class="form-control" placeholder="Skill name, e.g. HTML">
                         </div>
                       </div>
 
@@ -590,7 +682,7 @@ if ($result) {
 
                         <div class="form-group">
                           <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Skill proficiency, e.g. 90">
+                            <input name="skill_proficiency" type="text" class="form-control" placeholder="Skill proficiency, e.g. 90">
                             <span class="input-group-addon">%</span>
                           </div>
                         </div>
